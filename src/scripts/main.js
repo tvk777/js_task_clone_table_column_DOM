6ce390f631d602e539f1560fe3bca988775220e9
tvk777 <klimenko.tv7@gmail.com>
2025-12-09 16:43:04 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,31 @@
 /* eslint no-console: [,{ allow: ["warn", "log", "error"] }] */
 'use strict';
 
-const table = document.querySelector('table');
+const cloneAndInsert = (parentElement, childTag) => {
+  if (!parentElement) {
+    return;
+  }
 
-console.log(table);
+  const cloneCell = parentElement
+    .querySelector(`${childTag}:nth-child(2)`)
+    .cloneNode(true);
+  const lastCell = parentElement.querySelector(`${childTag}:last-child`);
+
+  if (cloneCell && lastCell) {
+    lastCell.before(cloneCell);
+  }
+};
+
+const headRow = document.querySelector('table thead tr');
+
+cloneAndInsert(headRow, 'th');
+
+const rows = document.querySelectorAll('table tbody tr');
+
+rows.forEach((row) => {
+  cloneAndInsert(row, 'td');
+});
+
+const footRow = document.querySelector('table tfoot tr');
+
+cloneAndInsert(footRow, 'th');
